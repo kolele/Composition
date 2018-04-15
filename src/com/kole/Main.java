80c8df5bf2807b821104e5098d8c75afb72ba605
Kole <slobodan.kosanovic@gmail.com>
2018-04-15 12:12:06 +0200

    Initial commit

---
+++ src/com/kole/Main.java
@@ -0,0 +1,17 @@
+package com.kole;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+	    Case theCase = new Case("220b", "Dell", "240", dimensions);
+
+	    Monitor monitor = new Monitor("27inch", "Acer", 27, new Resolution(2540, 1440));
+	    Motherboard theMotherboard = new Motherboard("lebac","acer",4,4,"somebios");
+
+	    PC thePC = new PC(theCase,monitor,theMotherboard);
+	    thePC.getMonitor().drawPixelAt(22,5,"blue");
+	    thePC.getMotherboard().loadProgram("Windows");
+	    thePC.getTheCase().pressPowerButton();
+    }
+}
---
+++ src/com/kole/Vehicle.java
@@ -0,0 +1,9 @@
+package com.kole;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
